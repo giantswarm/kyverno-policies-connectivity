20a1367a2e171c1449375bbf5c9c1124c9e3b7a9
Franco <franco@giantswarm.io>
2026-01-08 12:30:34 -0300

    Get rid of duplicated targets

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -23,20 +23,9 @@ verify:
 clean: ## Delete test manifests from kind cluster.
 	./hack/cleanup-local.sh
 
-.PHONY: kind-create
-kind-create: ## create kind cluster if needed
-	KIND_CLUSTER_NAME=$(KIND_CLUSTER_NAME) ./hack/kind-with-registry.sh
-# 	./hack/setup-kind.sh
-
 .PHONY: tilt-up
 tilt-up: ## Start Tilt
 	tilt up
-
-# If you change kyverno version here remember to change it in the Tiltfile too
-.PHONY: install-kyverno
-install-kyverno:
-	kubectl create --context kind-$(KIND_CLUSTER_NAME) -f https://raw.githubusercontent.com/kyverno/kyverno/v1.5.1/definitions/release/install.yaml
-	kubectl wait --context kind-$(KIND_CLUSTER_NAME) --for=condition=ready pod -l app=kyverno -nkyverno
 
 .PHONY: kind-get-kubeconfig
 kind-get-kubeconfig:
